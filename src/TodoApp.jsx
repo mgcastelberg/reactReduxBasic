15e52f55082db92ad992ee258a94d658c5273312
ManuxDark <mgcastelberg@hotmail.com>
2025-03-20 00:15:37 -0600

    Consumir el API mediante el custom hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useGetTodosQuery } from './store/apis/todosApi';
 
 export const TodoApp = () => {
+
+    const { data: todos, isLoading } = useGetTodosQuery(); //Hace peticion y almacena en cache
+    // console.log(todo);
   return (
     <>
         <h1>TodoApp - RTK Query</h1>
         <hr />
-        <h4>isLoading...</h4>
+        <h4>isLoading: { isLoading ? 'true' : 'false' }</h4>
 
         <pre>...</pre>
+
+        <ul>
+            {
+                todos?.map( todo => (
+                    <li style={{ textTransform: 'capitalize', textAlign: 'left' }} key={ todo.id }>
+                        { todo.completed ? '✅' : '❌' } - { todo.title }</li>
+                ))
+            }
+        </ul>
 
         <button>
             Next Todo
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './slices/counter/counterSlice'
 import { pokemonSlice } from './slices/pokemon/pokemonSlice'
+import { todosApi } from './apis/todosApi'
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    pokemons: pokemonSlice.reducer
+    pokemons: pokemonSlice.reducer,
+    [todosApi.reducerPath]: todosApi.reducer // llaves cuadradas indicando propiedad computada
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat( todosApi.middleware ),
 })
